6e4f948c100ac527698acaef4e096a3b32c0d85d
Gabriel <gabrielboetto@hotmail.com>
2023-04-07 17:08:50 -0300

    comienzo de backend

---
+++ ProyectoIntegrador_BoettoGabriel/BackEnd/mgb/src/main/java/com/portfolio/mgb/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.portfolio.mgb.Interface;
+
+import com.portfolio.mgb.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+            
+    //Eliminar un objeto
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
